8a060dfbea3e3a8f14597aacc390937194707186
sseungki98 <appp_le@naver.com>
2023-01-13 13:43:51 +0900

    feat: Create Player class's roll function
    generate random number and check validation

--- pig_dice_game.py
+++ pig_dice_game.py
@@ -1,4 +1,15 @@
+from random import randint
+
+
 class Player:
+
     def __init__(self):
         self.now_score = 0
         self.total_score = 0
+
+    def roll(self):
+        dice_number = randint(1,6)
+        if dice_number == 1:
+            return
+        else:
+            self.now_score += dice_number
